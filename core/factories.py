04d969d634318d60865f9de34f3db6025fd5d78d
whit3ez <whit3ez@gmail.com>
2023-05-24 12:27:38 +0500

    Обновлено ДЗ 10

---
+++ core/factories.py
@@ -0,0 +1,32 @@
+from faker import Factory
+import factory
+from factory import Sequence
+from core import models
+
+factory_ru = Factory.create('ru-Ru')
+
+class ProductCategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.ProductCategory
+
+    name = Sequence(lambda n: f"Category {n}")
+    description = factory_ru.text()
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Product
+
+    name = Sequence(lambda n: f"Product {n}")
+    description = factory_ru.text()
+    price = Sequence(lambda n: n * 10)
+    category = factory.SubFactory(ProductCategoryFactory)
+
+class ClientFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Client
+
+    name = factory_ru.name()
+    email = factory_ru.email()
+    phone = factory_ru.phone_number()
+
+
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from django.urls import reverse
+from core import factories
 
-# Create your tests here.
+class ProductListViewTest(TestCase):
+    def setUp(self):
+        self.url = reverse('core:product_list')
+
+    def test_product_list_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/product_list.html')
+
+    def test_product_list_context(self):
+        products = factories.ProductFactory.create_batch(3)
+        response = self.client.get(self.url)
+        self.assertEqual(list(response.context['products']), products)
+
+class ProductDetailViewTest(TestCase):
+    def setUp(self):
+        self.product = factories.ProductFactory()
+        self.url = reverse('core:product_detail', kwargs={'pk': self.product.pk})
+
+    def test_product_detail_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/product_detail.html')
+
+    def test_product_detail_context(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.context['product'], self.product)
+
+class ClientListViewTest(TestCase):
+    def setUp(self):
+        self.url = reverse('core:client_list')
+
+    def test_client_list_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/client_list.html')
+
+    def test_client_list_context(self):
+        clients = factories.ClientFactory.create_batch(3)
+        response = self.client.get(self.url)
+        self.assertEqual(list(response.context['clients']), clients)
